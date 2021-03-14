607f06cfcab42c6aeced5897223b0d79c39a3b78
navid-sheik <navidsheikh2020@gmail.com>
2021-03-14 19:14:33 +0000

    Router Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
 
+import {Link} from 'react-router-dom'
+
 const Footer = () => {
     return (
         <footer>
             <p>Copyright &copy; 2021</p>
-            <a href="/about">About </a>
+            <Link to="/about">About </Link>
         </footer>
 
     )
